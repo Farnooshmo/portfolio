9ad74438b4b4552401a51866304f1ae4af6692b3
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-08-25 23:55:19 +0100

    Add good-pr project to projects component

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,7 +1,10 @@
 export default function Projects() {
   return (
     <div>
-<p>Projects</p>
+      <p>Projects</p>
+      <div>
+        <img src="/good.png" alt="Good-PR" />
+      </div>
     </div>
-  )
-};
+  );
+}
