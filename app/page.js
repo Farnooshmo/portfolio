729fc678e99bdb77ae7b61c4ea5ec1e1f5cfd04b
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-09-15 15:02:19 +0100

    Update the bg-color

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-tr from-[rgb(18,35,60)] via-transparent to-[rgb(187,187,187)]">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-tr from-zinc-500  via-teal-1000 via-10% to-zinc-950 to-60% text-pink-300 ">
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Portfolio
