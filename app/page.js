2cd0a932637b76d017b9b8a8f5a2778b65008a37
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-09-21 00:00:18 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,34 +4,36 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-tr from-zinc-500  via-teal-1000 via-10% to-zinc-950 to-60% text-red-50 ">
       <div>
-        <div className="text-xl font-bold  mb-8">Farnoosh Moayeri</div>
-      </div>
-      <div>
-        <Image
-          src="/far.png"
-          width={200}
-          height={200}
-          alt="Farnoosh Moayeri"
-          className="display: block  rounded-full"
-        />
-      </div>
-      <h1 className="items-center text-pink-800 text-2xl font-semibold mb-8 md:text-red-50">
-        Full-Stack Developer
-      </h1>
-      <p className="whitespace-normal mb-8 text-base">
-        As a dynamic and forward-thinking professional, I hold a Bachelor of
-        Arts in Theatre and Performance and have gained valuable experience in
-        education and administration. However, my true passion lies in the world
-        of Web Development. Recently, I have decided to take on the exciting
-        challenge of changing my career path by enrolling in an intensive course
-        that covers both front-end and back-end aspects of web development.
-        Through this comprehensive course, I have honed my skills and completed
-        successful projects, gaining a sharp eye for detail and a creative
-        mindset that make me a valuable asset to any team. I am now eager to
-        apply my knowledge and expertise professionally and contribute to the
-        dynamic and ever-evolving world of web development.{" "}
-      </p>
+        <div className=" text-xl font-bold  mb-8">Farnoosh Moayeri</div>
 
+        <div>
+          <Image
+            src="/far.png"
+            width={200}
+            height={200}
+            alt="Farnoosh Moayeri"
+            className="rounded-full row-span-3"
+          />
+        </div>
+        <h1 className="items-center text-pink-800 text-2xl font-semibold mb-8 md:text-red-50">
+          Full-Stack Developer
+        </h1>
+        <p className=" grid whitespace-normal mb-8 text-base">
+          As a dynamic and forward-thinking professional, I hold a Bachelor of
+          Arts in Theatre and Performance and have gained valuable experience in
+          education and administration. However, my true passion lies in the
+          world of Web Development. Recently, I have decided to take on the
+          exciting challenge of changing my career path by enrolling in an
+          intensive course that covers both front-end and back-end aspects of
+          web development. Through this comprehensive course, I have honed my
+          skills and completed successful projects, gaining a sharp eye for
+          detail and a creative mindset that make me a valuable asset to any
+          team. I am now eager to apply my knowledge and expertise
+          professionally and contribute to the dynamic and ever-evolving world
+          of web development.{" "}
+        </p>
+      </div>
+      <section></section>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="/projects"
