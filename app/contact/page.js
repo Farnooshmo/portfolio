2ca4ed01d86b854ae3340b25c6b65dfa2db3993f
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-09-28 12:58:09 +0100

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className="flex flex-col justify-items-center items-center py-20 bg-gradient-to-tr from-zinc-600 to-zinc-950 h-screen text-zinc-700">
       <h1 className="text-red-50 text-xl hover:text-2xl">
-      Let's chat! Tell me about your project! 🤍
+        Let's chat! Tell me about your project! 🤩
       </h1>
       <form className="mt-8 ml-8 w-full max-w-lg justify-items-center border-y-4 border-pink-800/50 hover:border-pink-800">
         <div className="flex flex-wrap mb-6 mt-2 ">
@@ -33,17 +33,17 @@ export default function Contact() {
           <label htmlFor="message" className="sr-only">
             Message
           </label>
-          <input
+          <textarea
             type="text"
             name="message"
             id="message"
             placeholder="Your Message"
-            className="basis-1/4 px-4 py-2 border rounded-md  hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-pink-800"
+            className=" px-4 py-2 border rounded-md  hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-pink-800"
           />
         </div>
         <button
           type="submit"
-          className="h-1/5 py-2 px-4 bg-blue-500 text-white rounded-3xl hover:bg-pink-800 mb-4"
+          className=" basis-1/4 py-2 px-4 bg-blue-500 text-white rounded-3xl hover:bg-pink-800 mb-2"
         >
           Send Message
         </button>
