3c8402e134a68963da0612969011fbc679134306
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-09-28 23:30:00 +0100

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,14 @@
+import { sendEmail } from "@/utils/email";
 
 export default async function handler(req, res) {
-  await sendEmail(req.body);
+  const { email, ...formData } = req.body; 
 
-  res.status(200).json({ success: true });
+  try {
+    await sendEmail(formData, email); 
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ success: false, error: "Email sending failed" });
+  }
 }
