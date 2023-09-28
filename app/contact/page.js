b422a8927fa0539e947a1585f14862999ec96beb
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-09-28 19:48:58 +0100

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,20 +1,36 @@
+"use client"
+import { useState } from "react";
+
 export default function Contact() {
+  // async function handleSubmit(event: any){
+  //   event.preventDefault()
+  //   const data ={
+  //     name: String(event.name.target.value),
+  //     email: String(event.email.target.value),
+  //     message: String(event.massage.target.value),
+  //   }
+  // }
   return (
     <div className="flex flex-col justify-items-center items-center py-20 bg-gradient-to-tr from-zinc-600 to-zinc-950 h-screen text-zinc-700">
       <h1 className="text-red-50 text-xl hover:text-2xl">
         Let's chat! Tell me about your project! 🤩
       </h1>
-      <form className="mt-8 ml-8 w-full max-w-lg justify-items-center border-y-4 border-pink-800/50 hover:border-pink-800">
+      <form onSubmit={handleSubmit} className="mt-8 ml-8 w-full max-w-lg justify-items-center border-y-4 border-pink-800/50 hover:border-pink-800">
         <div className="flex flex-wrap mb-6 mt-2 ">
           <label htmlFor="name" className="sr-only">
             Name
           </label>
           <input
+            // value={name}
+            // onChange={(e:FormEvent) => setName(e.preventdefault())}
+            minLength={2}
+            maxLength={15}
+            required
             type="text"
             name="name"
             id="name"
             placeholder="Your name"
-            className="basis-1/4  flex-col px-4 py-2 border rounded-md   focus:outline-none focus:ring-2 focus:ring-pink-800 hover:bg-slate-600"
+            className="basis-1/4  flex-col px-4 py-2 border rounded-md   focus:outline-none focus:ring-2 focus:ring-pink-800 "
           />
         </div>
         <div className="flex flex-wrap mb-6">
@@ -22,11 +38,16 @@ export default function Contact() {
             Email
           </label>
           <input
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
+            minLength={5}
+            maxLength={150}
+            required
             type="text"
             name="email"
             id="email"
             placeholder="Your email"
-            className="basis-1/4 px-4 py-2 border rounded-md hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-pink-800"
+            className="basis-1/4 px-4 py-2 border rounded-md  focus:outline-none focus:ring-2 focus:ring-pink-800"
           />
         </div>
         <div className="flex flex-wrap mb-6">
@@ -34,11 +55,16 @@ export default function Contact() {
             Message
           </label>
           <textarea
+            // value={message}
+            // onChange={(e) => setMessage(e.target.value)}
+            minLength={7}
+            maxLength={150}
+            required
             type="text"
             name="message"
             id="message"
             placeholder="Your Message"
-            className=" px-4 py-2 border rounded-md  hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-pink-800"
+            className=" px-4 py-2 border rounded-md   focus:outline-none focus:ring-2 focus:ring-pink-800"
           />
         </div>
         <button
