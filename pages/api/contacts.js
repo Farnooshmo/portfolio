9ea715767e018cbfdd846a9e1644260529c1b7fe
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-09-29 13:42:51 +0100

    change the file name

--- pages/api/contacts.js
+++ pages/api/contacts.js
@@ -1,10 +1,10 @@
 import { sendEmail } from "@/utils/email";
 
 export default async function handler(req, res) {
-  const { email, ...formData } = req.body; 
+  const { email, ...formData } = req.body;
 
   try {
-    await sendEmail(formData, email); 
+    await sendEmail(formData, email);
 
     res.status(200).json({ success: true });
   } catch (error) {
