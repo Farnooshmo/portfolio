8f3e89a5083c48e22d9b51c82b89932807cff58f
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-10-05 22:32:52 +0100

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,51 +1,75 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import Link from "next/link";
 
 const links = [
   { title: "Home", url: "/" },
+  // { title: "About", url: "/about" },
   { title: "Projects", url: "/projects" },
-  { title: "contact", url: "/contact" },
-  { title: "GitHub", url: "https://github.com/Farnooshmo" },
-  {
-    title: "Linkedin",
-    url: "https://www.linkedin.com/in/farnooshmoayeri/",
-  },
+  { title: "Contact", url: "/contact" },
 ];
+
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+
+      window.addEventListener("resize", () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      });
+    }
+  }, []);
+
+  const isMobile = windowSize.width < 768;
+
   return (
-    <nav>
-      <button
-        onClick={handleClick}
-        className="flex flex-col text-pink-800 z-1 ml-8 mt-4  w-40 h-20 "
-      >
-        <span
-          className={` text-2xl rounded-md bg-steel-500 block transition-all duration-300 ease-out 
-                    h-0.5 w-6 rounded-sm ${
-                      isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
-                    }`}
-        ></span>
-        <span
-          className={`text-2xl bg-steel-500 block transition-all duration-300 ease-out 
-                    h-0.5 w-6 rounded-sm my-0.5 ${
-                      isOpen ? "opacity-0" : "opacity-100"
-                    }`}
-        ></span>
-        <span
-          className={`text-2xl bg-steel-500 block transition-all duration-300 ease-out 
-                    h-0.5 w-6 rounded-sm ${
-                      isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
-                    }`}
-        ></span>
-      </button>
+    <nav className="bg-gray-800 text-white px-4 py-3 flex items-center justify-between">
+      <Link href="/">
+        Farnoos Moayeri
+      </Link>
+
+      {isMobile ? (
+        <button
+          className="rounded-md bg-gray-700 p-2"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </button>
+      ) : (
+        <ul className="flex gap-x-4">
+          {links.map((link) => (
+            <li key={link.title}>
+              <Link href={link.url}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+
       {isOpen && (
-        <ul className="nav-links">
-          {links.map((link, index) => (
-            <li key={index}>
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-25 z-10"></div>
+      )}
+
+      {isOpen && (
+        <ul className="fixed top-0 left-0 flex flex-col items-center self-end py-24 bg-white shadow-xl z-20">
+          {links.map((link) => (
+            <li
+              key={link.title}
+              className="text-lg font-medium text-gray-800 px-6 py-2"
+              onClick={() => setIsOpen(!isOpen)}
+            >
               <Link href={link.url}>{link.title}</Link>
             </li>
           ))}
