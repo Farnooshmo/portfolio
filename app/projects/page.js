ba9e955f4de2b9d3cecb4e88858e45034510926d
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-11-01 23:27:58 +0000

    Import Navigation, EffectCoverflow

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,84 +1,108 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "../globals.css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation" ;
 
+import SwiperCore, { Navigation, EffectCoverflow } from "swiper/core"; // Import necessary Swiper components
+SwiperCore.use([Navigation, EffectCoverflow]); // Use the components
 export default function Projects() {
-  return (
-    <div className="bg-gradient-to-tr from-zinc-600 to-zinc-950 h-screen text-red-50 h-full">
-      <div className="flex justify-between mb-4">
-        <h1 className="mt-8 ml-8 text-2xl"></h1>
-      </div>
+  const [activeIndex, setActiveIndex] = useState(0);
 
-      <div className="grid grid-cols-1 mx-4 my-4 md:grid-cols-2 gap-4">
-        <div className="flex flex-col  items-center bg-zinc-900 border-pink-800 border-2 rounded-lg p-4 hover:animate-pulse">
-          <Link href="https://good-pr.vercel.app/">
-            <div className="w-96 h-48 overflow-hidden  relative">
+  const onSlideChange = (swiper) => {
+    setActiveIndex(swiper.activeIndex);
+  };
+
+  return (
+    <div className="min-h-screen overflow-hidden bg-gradient-to-tr from-zinc-600 to-zinc-950 text-red-50">
+      <Swiper
+        effect={"coverflow"} // Set the coverflow effect
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={3}
+        loop={true} // Enable infinite loop
+        navigation={true} // Enable navigation arrows
+        className="relative mt-6"
+      >
+        <SwiperSlide>
+          <div className=" flex flex-col object-contain items-center justify-center w-1/4">
+            <Link href="https://good-pr.vercel.app/">
               <Image
                 src="/good-pr-project.png"
-                layout="fill"
-                objectFit="cover"
-                alt="Good-PR"
+                width={240}
+                height={120}
+                alt="Good-PR Project"
               />
-            </div>
-          </Link>
+            </Link>
+            <h1 className="mt-2 text-xs text-rose-50 text-center">
+              Teamwork GitHub PR Tracker
+            </h1>
+            <h2 className="mt-1 text-xs text-rose-50 text-center">
+              Next.JS, Tailwind CSS, Prisma
+            </h2>
+          </div>
+        </SwiperSlide>
 
-          <h1 className="mt-2 text-xl text-rose-50">
-            Teamwork GitHub Pull Request Tracker
-          </h1>
-          <h2 className="mt-1 text-lg text-rose-50">
-            Next.JS, Tailwind CSS, Prisma
-          </h2>
-        </div>
-
-        <div className="flex flex-col items-center bg-zinc-900 border-pink-800 border-2 rounded-lg p-4 hover:animate-pulse">
-          <Link href="https://final-project-assessment.netlify.app/">
-            <div className="w-96 h-48 relative">
+        <SwiperSlide>
+          <div className="flex flex-col object-contain w-1/4  justify-center items-center ">
+            <Link href="https://final-project-assessment.netlify.app/">
               <Image
-                src="/video-project.png"
-                layout="fill"
-                objectFit="cover"
-                alt="Video-recommendation"
+                src="/video-recommendation.png"
+                width={240}
+                height={120}
+                alt="Video Recommendation"
               />
-            </div>
-          </Link>
+            </Link>
+            <h1 className="mt-2 text-xs text-rose-50 text-center">
+              Video Recommendation
+            </h1>
+            <h2 className="mt-1 text-xs text-rose-50 text-center">
+              React.JS, Node.JS, PostgreSQL
+            </h2>
+          </div>
+        </SwiperSlide>
 
-          <h1 className="mt-2 text-xl text-rose-50">Video Recommendation</h1>
-          <h2 className="mt-1 text-lg text-rose-50">
-            React.JS, Node.JS, PostgreSQL
-          </h2>
-        </div>
-
-        <div className="flex flex-col items-center bg-zinc-900 border-pink-800 border-2 rounded-lg p-4 hover:animate-pulse">
-          <Link href="https://teamwork-candycrush.netlify.app/">
-            <div className="w-96 h-48 relative">
+        <SwiperSlide>
+          <div className="flex flex-col object-contain w-1/4 justify-center items-center">
+            <Link href="https://teamwork-candycrush.netlify.app/">
               <Image
                 src="/candy-project.png"
-                layout="fill"
-                objectFit="cover"
-                alt="Candy-crush"
+                width={240}
+                height={120}
+                alt="Candy Crush"
               />
-            </div>
-          </Link>
+            </Link>
+            <h1 className="mt-2 text-xs text-rose-50 text-center">
+              Teamwork Candy Crush
+            </h1>
+            <h2 className="mt-1 text-xs text-rose-50 text-center">
+              React.JS, Node.JS, CSS
+            </h2>
+          </div>
+        </SwiperSlide>
 
-          <h1 className="mt-2 text-xl text-rose-50">Teamwork Candy Crush</h1>
-          <h2 className="mt-1 text-lg text-rose-50">React.JS, Node.JS, CSS</h2>
-        </div>
-
-        <div className="flex flex-col items-center bg-zinc-900 border-pink-800 border-2 rounded-lg p-4 hover:animate-pulse">
-          <Link href="https://london9-amazon-clone-50-react-project.netlify.app/">
-            <div className="w-96 h-48 relative">
+        <SwiperSlide>
+          <div className=" flex flex-col object-contain w-1/4 items-center justify-center">
+            <Link href="https://london9-amazon-clone-50-react-project.netlify.app/">
               <Image
-                src="/ecommerce-project.png"
-                layout="fill"
-                objectFit="cover"
-                alt="e-shop"
+                src="/eshop-project.png"
+                width={240}
+                height={120}
+                alt="E-shop"
               />
-            </div>
-          </Link>
-
-          <h1 className="mt-2 text-xl text-rose-50">Teamwork E-commerce</h1>
-          <h2 className="mt-1 text-lg text-rose-50">React.JS, Firebase</h2>
-        </div>
-      </div>
+            </Link>
+            <h1 className="mt-2 text-xs text-rose-50 text-center">
+              Teamwork E-commerce
+            </h1>
+            <h2 className="mt-1 text-xs text-rose-50 text-center">
+              React.JS, Firebase
+            </h2>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
