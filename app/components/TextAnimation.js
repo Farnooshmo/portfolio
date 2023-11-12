e0142fc08de87b71dc0e9bd3b0c72dab42925ebf
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-11-12 03:07:01 +0000

    Change the bg color and font weight

--- app/components/TextAnimation.js
+++ app/components/TextAnimation.js
@@ -1,5 +1,5 @@
 // AnimatedText.jsx
-
+"use client"
 import { useEffect, useState } from "react";
 
 export default function AnimatedText() {
@@ -18,7 +18,7 @@ export default function AnimatedText() {
   const duration = "duration-[5s] ease-out";
 
   return (
-    <div className="text-container relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-mono text-center text-2xl font-light">
+    <div className="text-container relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-violet-00  font-mono text-center text-xl font-extrabold">
       <span
         className={`reg-text ${loaded ? "" : fadeUp} ${
           loaded ? "opacity-100" : ""
