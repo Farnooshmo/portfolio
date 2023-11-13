884a5decd4c259f208ecf26964a63a376f363375
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-11-13 01:41:40 +0000

    add arrow icon, photo, link to the  main page +modify the styling of icons

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { FaLinkedin } from "react-icons/fa";
+import { FaArrowDown, FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
 import { FaFile } from "react-icons/fa";
@@ -7,6 +7,7 @@ import Contact from "./contact/page";
 import Link from "next/link";
 import TextAnimation from "./components/TextAnimation";
 import Projects from "./projects/page";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -14,23 +15,44 @@ export default function Home() {
       {/* ----ABOUT ME ---- */}
       <span className="sr-only">About me</span>
       <div
-        className="items-center text-center  font-medium mb-4 text-violet-800 
+        className=" flex flex-col items-center text-center hover:skew-y-14 mt-6 font-medium mb-4 text-violet-800 
         hover:animate-pulse "
         id="about"
       >
         <TextAnimation />
         Software Engineer
       </div>
-      <p className="whitespace-normal mb-8 text-base text-center text-zinc-700">
+      <div className="flex ">
+        <Image
+          src="/farnooshmoayeri.png"
+          width={100}
+          height={100}
+          alt="farnoosh"
+          className="fill-container w-full h-full rounded-full border-dashed"
+        />
+      </div>
+      <p className="  whitespace-normal mb-8 text-base text-center text-zinc-700 hover:resize">
         A dynamic, forward-thinking software developer with experience building
         full-stack web applications. As a recent graduate of a comprehensive
-        software development training course at 
-        <Link href="https://codeyourfuture.io/" className="text-pink-400"> CodeYourFuture</Link>, I am
-        looking to bring my creative mindset and sharp eye for detail to a
-        professional engineering team.
+        software development training course at
+        <Link href="https://codeyourfuture.io/" className="text-pink-500">
+          {" "}
+          CodeYourFuture
+        </Link>
+        , I am looking to bring my creative mindset and sharp eye for detail to
+        a professional engineering team.
       </p>
       {/* ---- PROJECTS ---- */}
-      <span className="sr-only">Projectss</span>
+      <span className="sr-only">Projects</span>
+      <span className="flex flex-row text-center  text-rose-400 text-2xl">
+        {" "}
+        My projects
+      </span>
+      <FaArrowDown
+        className="text-4xl mt-4 text-rose-300 hover:text-rose-700 
+               animate-bounce "
+      />
+
       <Projects />
 
       {/* ---- FOOTER ---- */}
@@ -44,8 +66,8 @@ export default function Home() {
           <div className="flex">
             <Link href="https://www.linkedin.com/in/farnooshmoayeri/">
               <FaLinkedin
-                className="text-2xl hover:text-zinc-800 hover:bg-white  
-               hover:animate-bounce"
+                className="text-2xl hover:text-zinc-700  
+               hover:animate-bounce "
               />{" "}
             </Link>
           </div>
@@ -53,8 +75,8 @@ export default function Home() {
           <div className="flex">
             <Link href="https://github.com/farnooshmo">
               <FaGithub
-                className="text-2xl hover:text-zinc-800 hover:bg-white 
-               hover:animate-bounce hover:rounded-xl"
+                className="text-2xl hover:text-zinc-700 
+               hover:animate-bounce "
               />
             </Link>
           </div>
@@ -62,8 +84,8 @@ export default function Home() {
           <div className="flex">
             <Link href="https://formsubmit.co/el/dehuci">
               <FaEnvelope
-                className="text-2xl hover:text-zinc-800 hover:bg-white 
-               hover:animate-bounce hover:rounded-xl"
+                className="text-2xl hover:text-zinc-700 
+               hover:animate-bounce "
               />
             </Link>
           </div>
@@ -71,8 +93,8 @@ export default function Home() {
           <div className="flex">
             <Link href="https://docs.google.com/document/d/1pYgGkQfO4wpeaESmL_OIzRFKdIWbL-ze/edit?usp=sharing&ouid=107146459050970629203&rtpof=true&sd=true">
               <FaFile
-                className="text-2xl hover:text-zinc-800 hover:bg-white 
-               hover:animate-bounce hover:rounded-xl"
+                className="text-2xl hover:text-zinc-700  
+               hover:animate-bounce"
               />
             </Link>
           </div>
