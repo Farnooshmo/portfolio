009c18914ece98bf7ec71b9bfa68a3e871306411
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-11-29 03:37:38 +0000

    Add bg image to the home page

--- app/page.js
+++ app/page.js
@@ -1,32 +1,42 @@
 "use client";
-import { FaArrowDown, FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
 import Contact from "./contact/page";
 import Link from "next/link";
 import TextAnimation from "./components/TextAnimation";
 import Projects from "./projects/page";
 import Image from "next/image";
+import background from "/public/background.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24 bg-stone-300  text-cyan-900 scroll-smooth ">
+    <main className="relative flex min-h-screen flex-col items-center justify-between p-6 md:p-24 bg-stone-300 text-cyan-900 scroll-smooth">
+      {/* Background Image */}
+      <Image
+        alt="Background"
+        src={background}
+        placeholder="empty"
+        quality={100}
+        priority
+        sizes="100vw"
+        className="absolute inset-0 w-full h-full object-cover opacity-50"
+      />
+
       {/* ----ABOUT ME ---- */}
       <span className="sr-only">About me</span>
       <div
-        className=" flex flex-col items-center text-center mt-10 font-light mb-10 text-violet-800 text-4xl
+        className="z-10 flex flex-col items-center text-center mt-10 font-light mb-10 text-violet-800 text-4xl
         hover:animate-pulse "
         id="about"
       >
         Farnoosh Moayeri
       </div>
-      <div className=" flex flex-col items-center text-center">
+      <div className="z-10 flex flex-col items-center text-center">
         {" "}
         <TextAnimation />
       </div>
 
-      <div className="flex flex-col mb-4 md:flex-row md:flex-wrap justify-around gap-4  border-dashed">
-        <div className="flex flex-col">
+      <div className=" z-10 flex flex-col mb-4 md:flex-row md:flex-wrap justify-around  border-dashed">
+        <div className="flex flex-col p-6">
           <Image
             src="/farnooshmoayeri.png"
             width={100}
@@ -35,8 +45,8 @@ export default function Home() {
             className="fill-container w-full h-full rounded-full border-dashed "
           />
         </div>
-        <div className=" flex flex-col items-center text-center  ">
-          <p className=" md:w-1/2 boarder-double border-2 border-red-300 rounded-xl whitespace-normal md:mb-6  p-10 text-base text-center text-zinc-900 hover:underline hover:underline-offset-6">
+        <div className="z-10 flex flex-col items-center text-center  ">
+          <p className=" md:w-1/2  boarder-double bg-stone-300 border-2 border-red-300 rounded-xl whitespace-normal md:mb-6  p-6 text-base text-center text-zinc-900 hover:underline hover:underline-offset-6">
             A dynamic, forward-thinking software engineer with experience
             building full-stack web applications. As a recent graduate of a
             comprehensive software development training course at
@@ -54,7 +64,7 @@ export default function Home() {
       </div>
       {/* ---- PROJECTS ---- */}
       <span className="sr-only">Projects</span>
-      <span className="flex flex-row text-center  text--600 text-2xl">
+      <span className="z-10 flex flex-row text-center mt-4 text-slate-900 text-2xl">
         {" "}
         My projects
       </span>
@@ -64,7 +74,7 @@ export default function Home() {
 
       <Link href="#projects">
         <FaArrowDown
-          className="text-4xl mt-4 text-rose-300 
+          className="z-10 text-4xl mt-4 text-slate-900 
                animate-bounce "
         />
       </Link>
@@ -73,48 +83,14 @@ export default function Home() {
       {/* ---- FOOTER ---- */}
       <span className="sr-only"> Contact me via</span>
 
-      <footer className="flex flex-col items-center gap-x-2 mb-0">
-        <span className="sr-only">Contact me via; LinkedIn, GitHub, Email</span>
-        <Contact />
-        <div className="flex flex-row justify-center mt-4 ml-2 gap-4">
-          {/* ----LinkedIn icon---- */}
-          <span className="sr-only"> Contact me via LinkedIn</span>
-
-          <div className="flex">
-            <Link href="https://www.linkedin.com/in/farnooshmoayeri/">
-              <FaLinkedin
-                className="text-2xl hover:text-zinc-700  
-               hover:animate-bounce "
-              />{" "}
-            </Link>
-          </div>
-          {/* ----GitHub icon---- */}
-          <span className="sr-only"> Contact me via GitHub</span>
-
-          <div className="flex">
-            <Link href="https://github.com/farnooshmo">
-              <FaGithub
-                className="text-2xl hover:text-zinc-700 
-               hover:animate-bounce "
-              />
-            </Link>
-          </div>
-          {/* ----Email icon---- */}
-          <span className="sr-only"> Contact me via Email</span>
-
-          <div className="flex">
-            <Link href="https://formsubmit.co/el/dehuci">
-              <FaEnvelope
-                className="text-2xl hover:text-zinc-700 
-               hover:animate-bounce "
-              />
-            </Link>
-          </div>
-        </div>
-
+      <Contact />
+      <footer className=" z-10 flex flex-col items-center gap-x-2 mb-0">
         <span className="sr-only"> Footer</span>
-        <p className=" flex text-xs text-center text-stone-900 mt-8 mb-0 border-t-4 border-pink-400">
-          Made by Farnoosh, 2023
+        <p className=" flex text-xs text-center text-stone-900 mt-8 mb-0 border-t-4 border-stone-600 ">
+          Made by Farnoosh, 2023 Image by{" "}
+          <a href="https://www.freepik.com/free-vector/realistic-liquid-marble-background-with-gold_13118034.htm">
+            Freepik
+          </a>
         </p>
       </footer>
     </main>
