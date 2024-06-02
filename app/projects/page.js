beffd437442ecbc58fa9596ff5037a274a249cc8
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-06-02 12:29:24 +0100

    update the project link

--- app/projects/page.js
+++ app/projects/page.js
@@ -89,7 +89,7 @@ export default function Projects() {
 
               <Link
                 className="flex  text-slate-800 rounded  mt-4  text-xs uppercase   focus:outline-none focus:ring focus:ring-pink-300"
-                href="https://final-project-assessment.netlify.app/"
+                href="https://movie-farnoosh.netlify.app/"
               >
                 <FaArrowUpRightFromSquare className=" text-2xl " />
               </Link>
